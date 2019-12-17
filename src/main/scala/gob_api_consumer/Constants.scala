151a2af5a9d4579a488e4f23d229de42c9e1b17e
Andrés González <a.gonzalez.terres@gmail.com>
2019-12-17 11:18:27 +0100

    Add Gob api constants

---
+++ src/main/scala/gob_api_consumer/Constants.scala
@@ -0,0 +1,21 @@
+package gob_api_consumer
+
+object Constants {
+
+  object Url {
+    val DATOS_AGROINDUSTRIA_GOB_AR = "https://datos.agroindustria.gob.ar/api/3/action/datastore_search"
+  }
+
+
+  object ResourceId {
+
+    // Datos del desembarque de captura marítima por puerto, flota y especie
+    // para el período Enero 2010 a Diciembre 2018.
+    val DESEMBARQUE_DE_CAPTURA_DE_ESPECIES_MARÍTIMAS_2010_A_2018 = "1996a5ec-7075-4062-9a79-05868fc2a2e2"
+
+    // Datos del desembarque de captura marítima por puerto, flota y especie para el período Enero 2019 a la actualidad.
+    val DESEMBARQUE_DE_CAPTURA_DE_ESPECIES_MARÍTIMAS_2019 = "77a15b4a-71e1-4b81-9732-ae0b6863c8cc"
+
+  }
+
+}
