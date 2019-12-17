0a4713d8f0115fe014dede5582f7f5db5f64802b
Andrés González <a.gonzalez.terres@gmail.com>
2019-12-17 11:02:25 +0100

    Add Gob api types

---
+++ src/main/scala/gob_api_consumer/Types.scala
@@ -0,0 +1,46 @@
+package gob_api_consumer
+
+object Types {
+
+  case class RequestBody(
+    limit: Int,
+    offset: Int,
+    resource_id: ResourceId,
+  )
+
+  type ResourceId = String
+
+  case class ResponseBody(result: Result, success: Boolean)
+
+  case class Result(
+    resource_id: ResourceId,
+    _links: Links,
+    limit: Int,
+    offset: Int,
+    records: List[Record],
+    total: Int,
+  )
+
+  case class Links(next: URL, start: URL)
+
+  type URL = String
+
+  case class Record(
+    captura: Int,
+    categoria: String,
+    departamento: String,
+    departamento_id: Int,
+    especie: String,
+    especie_agrupada: String,
+    fecha: String,
+    flota: String,
+    latitud: Option[Degree],
+    longitud: Option[Degree],
+    provincia: String,
+    provincia_id: Int,
+    puerto: String,
+  )
+
+  type Degree = Float
+
+}
