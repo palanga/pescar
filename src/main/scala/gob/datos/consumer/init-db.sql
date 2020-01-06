aaf3acb717362a9349875db73a3da76769611e26
Andrés González <a.gonzalez.terres@gmail.com>
2020-01-06 10:11:43 +0100

    Rearange table fields

--- src/main/scala/gob/datos/consumer/init-db.sql
+++ src/main/scala/gob/datos/consumer/init-db.sql
@@ -1,19 +1,19 @@
 DROP TABLE IF EXISTS records;
 
--- TODO reorder fields
+-- TODO make primary key
 CREATE TABLE records
 (
-    captura          INT     NOT NULL,
-    categoria        VARCHAR NOT NULL,
-    departamento     VARCHAR NOT NULL,
-    departamento_id  INT     NOT NULL,
-    especie          VARCHAR NOT NULL,
-    especie_agrupada VARCHAR NOT NULL,
     fecha            VARCHAR NOT NULL,
     flota            VARCHAR NOT NULL,
-    latitud          FLOAT,
-    longitud         FLOAT,
+    puerto           VARCHAR NOT NULL,
     provincia        VARCHAR NOT NULL,
     provincia_id     INT     NOT NULL,
-    puerto           VARCHAR NOT NULL
+    departamento     VARCHAR NOT NULL,
+    departamento_id  INT     NOT NULL,
+    latitud          FLOAT,
+    longitud         FLOAT,
+    categoria        VARCHAR NOT NULL,
+    especie          VARCHAR NOT NULL,
+    especie_agrupada VARCHAR NOT NULL,
+    captura          INT     NOT NULL
 );
