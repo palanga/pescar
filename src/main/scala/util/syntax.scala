0ad57a63e833c55b8394e973e8216049d2e7e6cd
Andrés González <a.gonzalez.terres@gmail.com>
2020-01-27 19:27:08 +0100

    ZIO syntax for Iterable, Cats and Doobie

--- src/main/scala/util/syntax.scala
+++ src/main/scala/util/syntax.scala
@@ -1,7 +1,12 @@
 package util
 
-import zio.ZIO
+import cats.effect.{ IO => CatsIO }
+import doobie.ConnectionIO
+import zio.clock.Clock
+import zio.console.{ putStrLn, Console }
+import zio.interop.catz.taskEffectInstance
 import zio.stream.ZStream
+import zio.{ Runtime, ZIO }
 
 object syntax {
 
@@ -24,6 +29,66 @@ object syntax {
        */
       def mapMPar_[R1 <: R, E1 >: E, B](n: Int, f: A => ZIO[R1, E1, B]): ZStream[R1, E1, B] =
         self.mapMPar[R1, E1, B](n)(f)
+
+    }
+
+    implicit final class ZIOOps[-R, +E, +A](private val self: ZIO[R, E, A]) extends AnyVal {
+
+      def tapPrint(show: A => String): ZIO[Console with R, E, A] = self.tap(putStrLn _ compose show)
+
+      def tapPrintTimed(prefix: String = "Elapsed time: "): ZIO[Console with R with Clock, E, A] =
+        self.timed.tap(timed => putStrLn(prefix ++ timed._1.render)).map(_._2)
+
+      def times(n: Int): Iterable[ZIO[R, E, A]] = ZIO.replicate(n)(self)
+
+    }
+
+    implicit final class IterableOps[-R, +E, +A](private val self: Iterable[ZIO[R, E, A]]) extends AnyVal {
+
+      /**
+       * Evaluate each effect in the structure from left to right, and collect
+       * the results. For a parallel version, see `collectAllPar`.
+       */
+      def collectAll: ZIO[R, E, Iterable[A]] = ZIO collectAll self
+
+      /**
+       * Evaluate each effect in the structure in parallel, and collect
+       * the results. For a sequential version, see `collectAll`.
+       */
+      def collectAllPar: ZIO[R, E, Iterable[A]] = ZIO collectAllPar self
+
+      /**
+       * Evaluate each effect in the structure in parallel, and collect
+       * the results. For a sequential version, see `collectAll`.
+       *
+       * Unlike `collectAllPar`, this method will use at most `n` fibers.
+       */
+      def collectAllParN(n: Int): ZIO[R, E, Iterable[A]] = ZIO.collectAllParN(n)(self)
+
+    }
+
+    object interop {
+
+      object cats {
+
+        implicit final class ZIOOps[-R, +E <: Throwable, +A](private val self: ZIO[R, E, A]) extends AnyVal {
+
+          def toCatsIO(implicit runtime: Runtime[R]): CatsIO[A] = taskEffectInstance toIO self
+
+        }
+
+      }
+
+      object doobie {
+
+        implicit final class ZIOOps[-R, +E <: Throwable, A](private val self: ZIO[R, E, A]) extends AnyVal {
+
+          def toConnectionIO(implicit runtime: Runtime[R]): ConnectionIO[A] =
+            taskEffectInstance.toIO(self).to[ConnectionIO]
+
+        }
+
+      }
 
     }
 
