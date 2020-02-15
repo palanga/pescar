365f816ac6d5c713a4d25da4ea4bfc1468c4c93c
Andrés González <a.gonzalez.terres@gmail.com>
2020-02-15 19:00:44 +0100

    Add GeoLocation

---
+++ src/main/scala/util/GeoLocation.scala
@@ -0,0 +1,19 @@
+package util
+
+object GeoLocation {
+
+  import GeoDegree.{ Latitude, Longitude }
+
+  val zero = GeoLocation(Latitude(0), Longitude(0))
+
+  def from(latitude: Float, longitude: Float): Either[String, GeoLocation] = ???
+
+}
+
+case class GeoLocation(latitude: GeoDegree.Latitude, longitude: GeoDegree.Longitude)
+
+sealed trait GeoDegree extends Any with Product with Serializable
+object GeoDegree {
+  final case class Latitude(value: Float)  extends AnyVal with GeoDegree
+  final case class Longitude(value: Float) extends AnyVal with GeoDegree
+}
