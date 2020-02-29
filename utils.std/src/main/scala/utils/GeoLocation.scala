76c585ff5d3461de774696c4df80ef11ca0e3ade
Andrés González <a.gonzalez.terres@gmail.com>
2020-02-29 11:47:25 +0100

    Add Either zip methods and GeoLocation smart constructors

--- utils.std/src/main/scala/utils/GeoLocation.scala
+++ utils.std/src/main/scala/utils/GeoLocation.scala
@@ -1,12 +1,31 @@
 package utils
 
+import utils.syntax.either._
+
 object GeoLocation {
 
   import GeoDegree.{ Latitude, Longitude }
 
-  val zero = GeoLocation(Latitude(0), Longitude(0))
+  val zero: GeoLocation = GeoLocation(Latitude(0), Longitude(0))
 
-  def from(latitude: Float, longitude: Float): Either[String, GeoLocation] = ???
+  def fromPair(pair: (Latitude, Longitude)): GeoLocation = pair match {
+    case (latitude, longitude) => GeoLocation(latitude, longitude)
+  }
+
+  def fromFloat(latitude: Float, longitude: Float): Either[IllegalArgumentException, GeoLocation] =
+    (Latitude from latitude zipWithError (Longitude from longitude)) (_ + _)
+      .map(fromPair)
+
+  def fromFloatUnsafe(latitude: Float, longitude: Float): GeoLocation =
+    fromFloat(latitude, longitude).fold(throw _, identity)
+
+  def fromFloatPair(pair: (Float, Float)): Either[IllegalArgumentException, GeoLocation] = fromFloat(pair._1, pair._2)
+
+  def fromFloatPairUnsafe(pair: (Float, Float)): GeoLocation = fromFloatUnsafe(pair._1, pair._2)
+
+  implicit class ExceptionOps[E <: Throwable](val self: E) extends AnyVal {
+    def +(other: E): E = new Exception(self.getMessage ++ "\n" ++ other.getMessage).asInstanceOf[E]
+  }
 
 }
 
@@ -14,6 +33,25 @@ case class GeoLocation(latitude: GeoDegree.Latitude, longitude: GeoDegree.Longit
 
 sealed trait GeoDegree extends Any with Product with Serializable
 object GeoDegree {
-  final case class Latitude(value: Float)  extends AnyVal with GeoDegree
-  final case class Longitude(value: Float) extends AnyVal with GeoDegree
+
+  case class Latitude(value: Float) extends AnyVal with GeoDegree
+  case class Longitude(value: Float) extends AnyVal with GeoDegree
+
+  private val LATITUDE_MIN  = -90
+  private val LATITUDE_MAX  = 90
+  private val LONGITUDE_MIN = -180
+  private val LONGITUDE_MAX = 180
+
+  object Latitude {
+    def from(value: Float): Either[IllegalArgumentException, Latitude] =
+      if (LATITUDE_MIN <= value && value <= LATITUDE_MAX) Right(Latitude(value))
+      else Left(new IllegalArgumentException(s"Couldn't construct a Latitude from $value"))
+  }
+
+  object Longitude {
+    def from(value: Float): Either[IllegalArgumentException, Longitude] =
+      if (LONGITUDE_MIN <= value && value <= LONGITUDE_MAX) Right(Longitude(value))
+      else Left(new IllegalArgumentException(s"Couldn't construct a Longitude from $value"))
+  }
+
 }
--- utils.std/src/main/scala/utils/syntax/either.scala
+++ utils.std/src/main/scala/utils/syntax/either.scala
@@ -6,6 +6,22 @@ object either {
 
     def bimap[E1, A1](f: E => E1, g: A => A1): Either[E1, A1] = self.left.map(f).map(g)
 
+    def zip[E1 >: E, A1](other: Either[E1, A1]): Either[List[E1], (A, A1)] =
+      (self, other) match {
+        case (Right(a), Right(a1)) => Right((a, a1))
+        case (Left(e), Left(e1))   => Left(e :: e1 :: Nil)
+        case (Left(e), _)          => Left(e :: Nil)
+        case (_, Left(e1))         => Left(e1 :: Nil)
+      }
+
+    def zipWithError[E1 >: E, E2 >: E1, A1](other: Either[E1, A1])(f: (E, E1) => E2): Either[E2, (A, A1)] =
+      (self, other) match {
+        case (Right(a), Right(a1)) => Right((a, a1))
+        case (Left(e), Left(e1))   => Left(f(e, e1))
+        case (Left(e), _)          => Left(e)
+        case (_, Left(e1))         => Left(e1)
+      }
+
   }
 
 }
